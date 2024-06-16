e4c69d1d9c12ecac14f01200f33aa6ce9dda4833
pavelmeysak <pavelmeysak@gmail.com>
2024-06-16 21:39:53 +0300

    Add files via upload
    Домашнее задание по теме "Переменные"

---
+++ homework2.py
@@ -0,0 +1,6 @@
+number_of_homeworks = 12
+hours_spent = 1.5
+course = "Python"
+homework_time = hours_spent / number_of_homeworks
+print('Курс:', course + ",", "всего задач:" + str(number_of_homeworks) + ",", "затрачено часов:", str(hours_spent)
+      + ",", "среднее время выполнения", homework_time, "часа.")
